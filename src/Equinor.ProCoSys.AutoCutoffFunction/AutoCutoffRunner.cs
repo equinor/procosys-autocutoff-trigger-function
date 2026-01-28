1596465d60c7451f2c07987ba826ae1296e7c789
Richard Thor Fridriksson Dungal <54837611+richard1927@users.noreply.github.com>
2026-01-28 09:40:16 +0100

    Release to Prod - Isolated worker model (#16)

--- src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffRunner.cs
+++ src/Equinor.ProCoSys.AutoCutoffFunction/AutoCutoffRunner.cs
@@ -3,17 +3,14 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Equinor.ProCoSys.AutoCutoffFunction
+namespace Equinor.ProCoSys.AutoCutoffFunction;
+
+internal static class AutoCutoffRunner
 {
-    internal class AutoCutoffRunner
+    public static async Task<HttpStatusCode> RunAsync(string url, CancellationToken cancellationToken = default)
     {
-        public static async Task<HttpStatusCode> RunAsync(string url, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync(url, cancellationToken);
-                return response.StatusCode;
-            }
-        }
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url, cancellationToken);
+        return response.StatusCode;
     }
 }
